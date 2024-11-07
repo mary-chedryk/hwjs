d100bd19450c03b7c24cc3b53567b60340f7b0d9
Mariia Chedryk <mary.chedryk@gmail.com>
2024-11-07 15:42:04 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const result = 5 + 5 + "5"; //"105"
+
+console.log(result);
+
+
+const email = "mary.chedryk@gmail.com";
+
+console.log(email);
+console.log('mary.chedryk@gmail.com'.length);
+console.log(email.includes("@"));
+
+
+const fullName = "My" + " " + "name" + " " + "is";
+const name = fullName + "Victor";
+
+console.log(fullName);
+
+let userName = "Mariia Chedryk";
+let payment = 400;
+
+alert(userName + "! Досплати" + " " + payment + " " + "гривень.")
